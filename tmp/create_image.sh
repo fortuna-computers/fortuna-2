117fc0dc5075adafde5d32c3641f7c7761cbd013
André Wagner <WagnerAndre@JohnDeere.com>
2021-08-20 13:04:11 -0300

    .

--- tmp/create_image.sh
+++ tmp/create_image.sh
@@ -5,5 +5,5 @@ parted sdcard.img --script -- mklabel msdos                     # create label
 parted sdcard.img --script -- mkpart primary fat16 1MiB 100%    # create partition
 mkfs.vfat -F16 sdcard.img                                       # format disk
 # TODO - check boot size
-dd bs=1 seek=62 if=boot.txt of=sdcard.img conv=notrunc          # copy boot file to boot area (byte 62 onwards)
-mcopy -i sdcard.img hello.txt ::                                # copy file to image
+# dd bs=1 seek=62 if=boot.txt of=sdcard.img conv=notrunc          # copy boot file to boot area (byte 62 onwards)
+# mcopy -i sdcard.img hello.txt ::                                # copy file to image
