ce64ba1460b1ef511471be1cd4575c484d830b4f
André Wagner <andre.nho@gmail.com>
2021-08-30 18:57:01 -0300

    .

--- tmp/create_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-
-dd if=/dev/zero of=sdcard.img iflag=fullblock bs=1M count=256   # create a 16 MB disk
-parted sdcard.img --script -- mklabel msdos                     # create label
-parted sdcard.img --script -- mkpart primary fat32 1MiB 100%    # create partition
-mkfs.vfat -F32 sdcard.img                                       # format disk
-# TODO - check boot size
-# dd bs=1 seek=62 if=boot.txt of=sdcard.img conv=notrunc          # copy boot file to boot area (byte 62 onwards)
-# mcopy -i sdcard.img hello.txt ::                                # copy file to image
