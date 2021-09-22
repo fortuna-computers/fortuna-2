85ae9c3b90ec7bdcab9d49f8f5e822d9270d2cc1
André Wagner <WagnerAndre@JohnDeere.com>
2021-09-22 21:36:45 +0100

    .

--- firmware-new/main.c
+++ firmware-new/main.c
@@ -1,3 +1,51 @@
+#include "uart.h"
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <avr/sfr_defs.h>
+#include <util/delay.h>
+
+static void check_mcucsr();
+
 int main()
 {
+    _delay_ms(50);
+    uart_init();
+
+    check_mcucsr();
+
+    uart_putchar('@');
+
+    for(;;);
+}
+
+ISR(BADISR_vect)   // catch all interrupts
+{
+    uart_putchar('!');
+    uart_putchar('!');
+    uart_putchar('!');
+    for(;;);
+}
+
+static void check_mcucsr()
+{
+    if (MCUCSR == 0)
+        return;
+
+    uart_putchar('!');
+
+    if (bit_is_set(MCUCSR, PORF))
+        uart_putchar('#');
+    if (bit_is_set(MCUCSR, EXTRF))
+        uart_putchar('%');
+    if (bit_is_set(MCUCSR, BORF))
+        uart_putchar('~');
+    if (bit_is_set(MCUCSR, WDRF))
+        uart_putchar('W');
+    if (bit_is_set(MCUCSR, JTRF))
+        uart_putchar('J');
+
+    uart_putchar(' ');
+    MCUCSR = 0;
 }
