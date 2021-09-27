927701b9451e7664027170ed4444cfc4c3c2e986
André Wagner <WagnerAndre@JohnDeere.com>
2021-09-27 14:10:56 -0300

    .

--- firmware-new/z80.c
+++ firmware-new/z80.c
@@ -2,9 +2,22 @@
 
 #include <avr/io.h>
 
+#define set_RST()   PORTB |= (1 << PB0)
+#define clear_RST() PORTB &= ~(1 << PB0)
+#define set_CLK()   PORTD |= (1 << PD5)
+#define clear_CLK() PORTD &= ~(1 << PD5)
+
 void z80_init()
 {
+    DDRB |= (1 << PB0);     // RST
+    DDRD |= (1 << PD5);     // CLK
+    
     // reset (initialize as low)
-    DDRB |= (1 << PB0);
-    PORTB &= ~(1 << PB0);
+    clear_RST();
+    
+    // run 50 cycles
+    for (uint8_t i = 0; i < 50; ++i) {
+        set_CLK();
+        clear_CLK();
+    }
 }
